7baa5ec7a05e1fc8be98a11397540d02800ccf8c
Basir98 <basirram98@gmail.com>
2021-10-27 16:24:56 +0200

    the app is functional

--- app/src/main/java/com/example/assignment3/MapsActivity.java
+++ app/src/main/java/com/example/assignment3/MapsActivity.java
@@ -3,7 +3,10 @@ package com.example.assignment3;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +21,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     String cityName,strCityLat, strCityLong;
+    Button btnGoBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             strCityLat = getWeatherIntent.getStringExtra("city_lat");
             strCityLong = getWeatherIntent.getStringExtra("city_long");
 
-
         }catch (Exception e){ }
+
+        btnGoBack = (Button) findViewById(R.id.btnGoBackToMain);
+        btnGoBack.setBackgroundColor(Color.RED);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
